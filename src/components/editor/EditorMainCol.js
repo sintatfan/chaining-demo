7c9db890cf6bb593a424fe125713dacc3b96c659
indzztat <tat@indzz.com>
2022-07-10 20:39:15 +0800

    Editor - progress bar

--- src/components/editor/EditorMainCol.js
+++ src/components/editor/EditorMainCol.js
@@ -1,12 +1,47 @@
-import {Box, Button, Card, Group, Stack, Switch, Text, Title} from "@mantine/core";
+import {Box, Button, Card, Group, Popover, Skeleton, Stack, Switch, Text, Title, UnstyledButton} from "@mantine/core";
 import CodeEditor from "./CodeEditor";
-import {PlayerPlay} from "tabler-icons-react";
+import {Eye, PlayerPlay} from "tabler-icons-react";
 import {useState} from "react";
 
+function HistoryPoint({item}) {
+    const [opened, setOpened] = useState(false);
+
+    const target = (
+        <UnstyledButton onClick={() => setOpened((o) => !o)}>
+            <span></span>
+        </UnstyledButton>
+    );
+
+    return <Popover
+        opened={opened}
+        onClose={() => setOpened(false)}
+        target={target}
+        width={260}
+        position="bottom"
+        withArrow
+        className="history-chain-point"
+        styles={(theme) => ({arrow: {backgroundColor: theme.colors.dark[7]}, popover: {backgroundColor: theme.colors.dark[7]}})}
+    >
+        <Skeleton visible height={226} mb="xs">
+            Lorem ipsum dolor sit amet...
+            {/* other content */}
+        </Skeleton>
+
+        <Button fullWidth><Eye /> View</Button>
+    </Popover>;
+}
+
 function HistoryCard() {
+    const items = [1,2,3,4].map((item) => (
+        <HistoryPoint item={item} />
+    ));
+
     return (
         <Card py="xs">
             <Title order={5}>Previous Coding</Title>
+            <Group className="history-chain">
+                {items}
+            </Group>
         </Card>
     );
 }

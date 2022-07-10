9572f95cfc7b4680f4472374e774dfcff015c8f2
indzztat <tat@indzz.com>
2022-07-10 16:01:12 +0800

    Link header logo to the home page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,6 +2,7 @@ import {Container, Header} from "@mantine/core";
 import HeaderGuest from "./HeaderGuest";
 import HeaderUser from "./HeaderUser";
 import {logoDark} from "../../theme";
+import {Link} from "react-router-dom";
 
 function HeaderProfile() {
     // TODO: consider auth state
@@ -13,7 +14,9 @@ export default function AppHeader() {
     return (
         <Header height={78} className="app-header">
             <Container fluid className="app-header-container">
-                <img src={logoDark} alt="Logo" />
+                <Link to="/">
+                    <img src={logoDark} alt="Logo" />
+                </Link>
                 <HeaderProfile />
             </Container>
         </Header>

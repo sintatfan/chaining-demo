6a51fa39ad69f341ba79ac135119b0b5dd96341e
indzztat <tat@indzz.com>
2022-07-20 09:39:48 +0800

    hide data import tool

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import EditorPage from "./pages/Editor";
 import './plugins/firebase';
 import './plugins/dayjs';
 import ProjectHomePage from "./pages/ProjectHome";
-import ImportDataTool from "./pages/ImportDataTool";
+// import ImportDataTool from "./pages/ImportDataTool";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,7 +20,7 @@ root.render(
                   <Route index element={<HomePage />} />
                   <Route path="project/:projectId" element={<ProjectHomePage />} />
                   <Route path="project/:projectId/:nodeId" element={<EditorPage />} />
-                  <Route path="import-data" element={<ImportDataTool />} />
+                  {/*<Route path="import-data" element={<ImportDataTool />} />*/}
                   <Route path="*" element={<NotFound />} />
               </Route>
           </Routes>
